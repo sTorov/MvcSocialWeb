fa416ef564b3088ea415cde8b00d27b0a55da3de
sTorov <sotorov2011@yandex.ru>
2023-01-24 17:42:35 +0300

    Добавлена библиотека классов MvcSocialWeb.Data Добавлен класс контеста базы данных SocialWebContextAdding MvcSocialWeb.Data

---
+++ MvcSocialWeb.Data/SocialWebContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcSocialWeb.Data
+{
+    public class SocialWebContext : DbContext
+    {
+        public SocialWebContext(DbContextOptions<SocialWebContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
