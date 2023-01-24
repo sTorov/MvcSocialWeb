d6907d44c5a2f282beefcd108483a48c6fe476a9
sTorov <sotorov2011@yandex.ru>
2023-01-24 16:51:01 +0300

    Добавьте файлы проекта.

---
+++ MvcSocialWeb/Program.cs
@@ -0,0 +1,38 @@
+namespace MvcSocialWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Services
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            //Configs
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+
+        }
+    }
+}
