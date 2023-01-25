a9b2655ff7152ca10e286cc4b193dcf044d3b8e4
sTorov <sotorov2011@yandex.ru>
2023-01-25 20:26:15 +0300

    MvcSocialWeb: Добавлен метод регистрации пользователя для RegisterController

--- MvcSocialWeb/Controllers/RegisterController.cs
+++ MvcSocialWeb/Controllers/RegisterController.cs
@@ -1,19 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MvcSocialWeb.Data.DBModel;
 using MvcSocialWeb.ViewModels;
 using MvcSocialWeb.ViewModels.Account;
 using System.Diagnostics;
 
 namespace MvcSocialWeb.Controllers
 {
+    /// <summary>
+    /// Контроллер регистрации
+    /// </summary>
     public class RegisterController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public RegisterController(IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        /// <summary>
+        /// Первый этап регистрации
+        /// </summary>
         [Route("Register")]
         [HttpGet]
         public IActionResult Register()
         {
-            return View("Views/Shared/Register.cshtml");
+            return View("Home/Register");
         }
 
+        /// <summary>
+        /// Второй этап регистрации
+        /// </summary>
         [Route("RegisterPart2")]
         [HttpGet]
         public IActionResult RegisterPart2(RegisterViewModel model)
@@ -21,6 +44,35 @@ namespace MvcSocialWeb.Controllers
             return View("RegisterPart2", model);
         }
 
+        /// <summary>
+        /// Регистрация нового пользователя в базе данных
+        /// </summary>
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid) 
+            {
+                var user = _mapper.Map<User>(model);
+                var result = await _userManager.CreateAsync(user, model.PasswordReg);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user,  false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError(string.Empty, error.Description);                    
+                }
+            }
+            return View("RegisterPart2", model);
+        }
+
+        /// <summary>
+        /// Вывод страницы с ошибкой
+        /// </summary>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
