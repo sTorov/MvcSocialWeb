ef16cc985a6c1c1a0c7b60d1be6f1671b6a29b88
sTorov <sotorov2011@yandex.ru>
2023-01-25 20:36:35 +0300

    MvcSocialWeb: Добавлен контроллер AccountManagerController

---
+++ MvcSocialWeb/Controllers/AccountManagerController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MvcSocialWeb.Data.DBModel;
+
+namespace MvcSocialWeb.Controllers
+{
+    /// <summary>
+    /// Контроллер аккаунтов
+    /// </summary>
+    public class AccountManagerController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountManagerController(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
