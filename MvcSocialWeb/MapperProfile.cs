47fa5ebe30443c8783664028fef77f0b32f4c66f
sTorov <sotorov2011@yandex.ru>
2023-01-25 21:44:16 +0300

    MvcSocialWeb: Добавлен маппинг из RegistrationViewModel и LoginViewModel в User

--- MvcSocialWeb/MapperProfile.cs
+++ MvcSocialWeb/MapperProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using MvcSocialWeb.Data.DBModel;
+using MvcSocialWeb.ViewModels.Account;
 
 namespace MvcSocialWeb
 {
@@ -9,7 +11,15 @@ namespace MvcSocialWeb
     {
         public MapperProfile()
         {
+            CreateMap<RegisterViewModel, User>()
+                .ForMember(user => user.Email, opt => opt.MapFrom(model => model.EmailReg))
+                .ForMember(user => user.UserName, opt => opt.MapFrom(model => model.Login))
+                .ForMember(user => user.BirthDate, opt => opt.MapFrom(model => new DateTime(model.Year, model.Month, model.Date)))
+                .ForMember(user => user.PasswordHash, opt => opt.MapFrom(model => model.PasswordReg.GetHashCode()));
 
+            CreateMap<LoginViewModel, User>()
+                .ForMember(user => user.Email, opt => opt.MapFrom(model => model.UserEmail))
+                .ForMember(user => user.PasswordHash, opt => opt.MapFrom(model => model.Password.GetHashCode()));
         }
     }
 }
