9e4072969b35b9694a4ddc501c6250b671295d47
sTorov <sotorov2011@yandex.ru>
2023-01-28 00:21:03 +0300

    Доработаны селекты даты рождения (JS, стили)

--- MvcSocialWeb/wwwroot/js/main-page.js
+++ MvcSocialWeb/wwwroot/js/main-page.js
@@ -3,15 +3,24 @@ const ddlYear = document.getElementById("ddlYear");
 const ddlDay = document.getElementById("ddlDay");
 
 function PopulateDays() {
+    const ddlDay = document.getElementById("ddlDay");
+
     const y = ddlYear.options[ddlYear.selectedIndex].value;
     const m = ddlMonth.options[ddlMonth.selectedIndex].value;
 
     if (m != 0 && y != 0) {
         const dayCount = 32 - new Date(y, m - 1, 32).getDate();
-        ddlDay.options.length = 0;
+
+        if(ddlDay.options[0].value == 0)
+            ddlDay.options.length = 1;
+        else 
+            ddlDay.options.length = 0;
+
         for (let i = 1; i <= dayCount; i++) {
             AddOption(ddlDay, i, i);
         }
+
+        ddlDay.classList.remove('input-validation-error');
     }
 }
 
@@ -22,7 +31,58 @@ function AddOption(ddl, text, value) {
     ddl.options.add(opt);
 }
 
-function RemoveFirstOptionForYearAndMonth() {
-    ddlMonth.options.remove(0);
-    ddlYear.options.remove(0);
+function RemoveOption(index) {
+    if(index > 0)
+        ddlDay.options.remove(index);
+
+    ddlMonth.options.remove(index);
+    ddlYear.options.remove(index);
+}
+
+function FirstSelectOption(select, callback) {
+    select.style.color = '#212529';
+
+    if (select == ddlDay) {
+        for (let i = 0; i < select.options.length; i++) {
+            if (select.options[i].value == 1) {
+                select.options.selectedIndex = i;
+                break;
+            }
+        }
+    }
+
+    RemoveEventListener(select, callback);
+}
+
+function RemoveEventListener(select, callback) {
+    select.removeEventListener("change", callback);
+    select.removeEventListener("DOMNodeInserted", callback)
+}
+
+function AddDefaultOption(){
+    const values = ['Day', 'Month', 'Year'];
+    const selected = [ddlDay, ddlMonth, ddlYear];
+
+    for (let i = 0; i < 3; i++) {
+        let opt = document.createElement('option');
+        opt.setAttribute('selected', 'selected');
+        opt.setAttribute('disabled', 'disabled');
+        opt.classList.add("default-opt");
+        opt.innerText = values[i];
+        opt.setAttribute('value', 0);
+        selected[i].insertAdjacentElement('afterbegin', opt);
+    }
+}
+
+const ddlDaysFirstSelect = () => FirstSelectOption(ddlDay, ddlDaysFirstSelect);
+const ddlMonthFirstSelect = () => FirstSelectOption(ddlMonth, ddlMonthFirstSelect);
+const ddlYearFirstSelect = () => FirstSelectOption(ddlYear, ddlYearFirstSelect);
+
+ddlMonth.addEventListener("change", PopulateDays);
+ddlYear.addEventListener("change", PopulateDays);
+
+function AddChangeStyleEvent() {
+    ddlDay.addEventListener("DOMNodeInserted", ddlDaysFirstSelect);
+    ddlMonth.addEventListener("change", ddlMonthFirstSelect);
+    ddlYear.addEventListener("change", ddlYearFirstSelect);
 }
