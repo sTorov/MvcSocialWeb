eecd9915a05430a573435e7f3f7e8d403ffade71
sTorov <sotorov2011@yandex.ru>
2023-01-28 00:58:48 +0300

    Изменена модель User

--- MvcSocialWeb.Data/DBModel/User.cs
+++ MvcSocialWeb.Data/DBModel/User.cs
@@ -11,5 +11,19 @@ namespace MvcSocialWeb.Data.DBModel
         public string? LastName { get; set; }
         public string? MiddleName { get; set; }
         public DateTime? BirthDate { get; set; }
+        public string Image { get; set; }
+        public string Status { get; set; }
+        public string About { get; set; }
+
+        public User() { }
+
+        public User(string image, string status, string about)
+        {
+            Image = image;
+            Status = status;
+            About = about;
+        }
+
+        public string GetFullName() => $"{FirstName} {MiddleName} {LastName}";
     }
 }
