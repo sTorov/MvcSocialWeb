92a568a9c4b42ae92c496051c2a6137a2fccc3b3
twoDevAcc <sotordima2000@gmail.com>
2023-02-01 16:06:18 +0300

    Добавлены модели данных для чата и нового сообщения

---
+++ MvcSocialWeb/ViewModels/Messages/ChatViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MvcSocialWeb.Data.DBModel.Messages;
+using MvcSocialWeb.Data.DBModel.Users;
+
+namespace MvcSocialWeb.ViewModels.Messages
+{
+    /// <summary>
+    /// Модель данных чата
+    /// </summary>
+    public class ChatViewModel
+    {
+        public User Sender{ get; set; }
+        public User Recipient { get; set; }
+        public List<Message> History { get; set; }
+        public MessageViewModel NewMessage { get; set; }
+
+        public ChatViewModel() 
+        {
+            NewMessage = new MessageViewModel();
+        }
+    }
+}
---
+++ MvcSocialWeb/ViewModels/Messages/MessageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcSocialWeb.ViewModels.Messages
+{
+    /// <summary>
+    /// Модель данных сообщения
+    /// </summary>
+    public class MessageViewModel
+    {
+        public string Text { get; set; }
+    }
+}
